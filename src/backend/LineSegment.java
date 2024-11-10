26f3bd761743983e4ec20474a2c9bc8edb8ed7ad
Abdelrahman Essam <70455708+AbdelrahmanEssam1007@users.noreply.github.com>
2024-11-10 11:14:59 +0200

    Update LineSegment.java

--- src/backend/LineSegment.java
+++ src/backend/LineSegment.java
@@ -3,15 +3,15 @@ package backend;
 import java.awt.*;
 
 public class LineSegment extends AbsShape {
-    public LineSegment() {
-        super();
-        getProperties().put("x2", 0.0);
-        getProperties().put("y2", 0.0);
-    }
+  public LineSegment() {
+    super();
+    getProperties().put("x2", 0.0);
+    getProperties().put("y2", 0.0);
+  }
 
-    @Override
-    public void draw(Graphics canvas) {
-        canvas.setColor(getColor());
-        canvas.drawLine(getPosition().x, getPosition().y, getProperties().get("x2").intValue(), getProperties().get("y2").intValue());
-    }
+  @Override
+  public void draw(Graphics canvas) {
+    canvas.setColor(getColor());
+    canvas.drawLine(getPosition().x, getPosition().y, getProperties().get("x2").intValue(), getProperties().get("y2").intValue());
+  }
 }
